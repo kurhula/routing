a722386a89b33d83ec0d33bab405ee3db3699dca
maqi <maqi@users.noreply.github.com>
2020-06-30 13:12:06 +0000

    deploy: ce586c9979956cae61264bc8472dbcf5473d8ed6

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["routing"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"routing/struct.NodeConfig.html\" title=\"struct routing::NodeConfig\">NodeConfig</a>","synthetic":false,"types":["routing::node::NodeConfig"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
